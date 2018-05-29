9019971b18c06d21662ef20e9fb2d2aca3f1c6fa
Malik Ericson <malik@Maliks-MacBook-Pro.local>
2018-05-29 11:42:00 +0200

    rigtig meget

--- header.js
+++ header.js
@@ -1,9 +1,10 @@
 window.addEventListener("load", hentHeader);
 
-async function henHeader() {
+async function hentHeader() {
     let headerDOM = document.querySelector("#header");
     let headerHentes = await fetch("header.html");
     let header = await headerHentes.text();
 
     headerDOM.innerHTML = header;
+
 }
