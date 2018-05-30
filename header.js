f80a926987358216365426ed60d0f5d1832b7fd2
Malik Ericson <malik@Maliks-MacBook-Pro.local>
2018-05-30 11:54:00 +0200

    oki

--- header.js
+++ header.js
@@ -16,3 +16,12 @@ $(window).scroll(function () {
         $('.logo').fadeIn();
     }
 });
+
+$(window).ready(function () {
+    setTimeout(function () {
+        $(".container_drop").click(function () {
+            $(".container_drop").toggleClass("change");
+            $(".dropdown").slideToggle();
+        });
+    });
+});
